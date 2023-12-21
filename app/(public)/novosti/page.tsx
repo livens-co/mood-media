b385fa26dd4835c83113247b513502248caad071
dcvijetovic <dominik@mintandlime.eu>
2023-12-21 20:08:26 +0100

    novosti

--- app/(public)/novosti/page.tsx
+++ app/(public)/novosti/page.tsx
@@ -1,9 +1,40 @@
-import './style.scss'
+import ArticleCard from "@/components/ArticleCard";
+import "./style.scss";
+
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
 const NewsPage = () => {
   return (
-    <div>NewsPage</div>
-  )
-}
+    <div className="newsPage">
+      <div className="newsGrid">
+        <div className="title">
+          <h1>Novosti</h1>
+          <h2>Zanimljivosti iz svijeta naših kreatora</h2>
+        </div>
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+      </div>
 
-export default NewsPage
+      <div className="pagination">
+        <button>
+          <FaArrowLeftLong />
+        </button>
+        <div className="page">1</div>
+        <div className="page">2</div>
+        <div className="page">3</div>
+        <div className="page">4</div>
+        <button>
+          <FaArrowRightLong />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewsPage;
