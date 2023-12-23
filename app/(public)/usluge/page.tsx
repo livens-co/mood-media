f99f932e95a0e91b2556289bae29794220b30887
dcvijetovic <dominik@mintandlime.eu>
2023-12-23 13:24:38 +0100

    usluge

--- app/(public)/usluge/page.tsx
+++ app/(public)/usluge/page.tsx
@@ -1,9 +1,42 @@
-import './style.scss'
+import ServicesCarousel from "@/components/ServicesCarousel";
+import "./style.scss";
+import Contact from "@/components/Contact";
 
 const ServicesPage = () => {
   return (
-    <div>ServicesPage</div>
-  )
-}
+    <div className="servicesPage">
+      <div className="header">
+        <h1>
+          Naši kreatori, vaša publika, <br />
+          vrhunski rezultati
+        </h1>
+      </div>
 
-export default ServicesPage
+      {/* TEXT */}
+      <div className="text">
+        <h1>Naše usluge</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+          totam, dolor culpa a commodi ipsam voluptatem suscipit possimus
+          molestiae praesentium eius fuga sapiente molestias excepturi adipisci
+          quia accusamus officiis perferendis cumque quod, ratione distinctio
+          eveniet beatae. Sapiente quae, aut laboriosam nulla nemo eos quas nisi
+          saepe quo tempore minima. Tempora nam dignissimos perspiciatis sint
+          reiciendis nostrum autem minus labore quae libero necessitatibus optio
+          laboriosam totam expedita iusto cum voluptate numquam aut molestias
+          consectetur error sed, rerum fugiat! Perferendis, molestiae dolore.
+        </p>
+      </div>
+
+      {/* CAROUSEL */}
+
+      <ServicesCarousel />
+
+      {/* CONTACT */}
+
+      <Contact />
+    </div>
+  );
+};
+
+export default ServicesPage;
