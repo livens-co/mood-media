3a414b770c6c606c3d4bdc8e4d7f9762fde236b8
dcvijetovic <dominik@mintandlime.eu>
2024-01-14 21:27:12 +0100

    mobile

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Navbar />
         <div className="mainLayout">{children}</div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
