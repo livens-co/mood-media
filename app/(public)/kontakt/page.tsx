2299aac56a88e44492ce00a35f581c0f4411cb48
dcvijetovic <dominik@mintandlime.eu>
2024-01-15 17:22:38 +0100

    resend

--- app/(public)/kontakt/page.tsx
+++ app/(public)/kontakt/page.tsx
@@ -1,14 +1,50 @@
+'use client'
+
 import ContactAnimation from "@/components/ContactAnimation";
 import "./style.scss";
 
 import { IoMail, IoPerson } from "react-icons/io5";
-import getArticles from "@/sanity/actions/get-articles";
-import getArticlesByCreator from "@/sanity/actions/get-articles-by-creator";
-import { Article, Creator } from "@/types";
+import { Resend } from "resend";
+import { ContactTemplate } from "@/components/EmailTemplates/ContactTemplate";
+import { useState } from "react";
 
-const ContactPage = async () => {
-  // const filteredArticles: Article[] = await getArticlesByCreator(4);
-  // console.log(filteredArticles);
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+
+
+const ContactPage =  () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Perform any form validation here if needed
+
+    try {
+      // Send the email using your existing logic
+      const data = await resend.emails.send({
+        from: 'Mood Media <noreply@moodmedia.hr>',
+        to: 'dominik@mintandlime.eu',
+        bcc: 'dominik@mintandlime.eu',
+        subject: 'Upit zaprimljen',
+        react: ContactTemplate({ firstName: name }),
+        text: '', // Add an empty string for the 'text' property
+      });
+
+      console.log('Email sent successfully:', data);
+
+      // Optionally, you can reset the form fields after successful submission
+      setName('');
+      setEmail('');
+      setMessage('');
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  };
+
 
 
   return (
@@ -21,7 +57,7 @@ const ContactPage = async () => {
           <h2>Kontaktirajte nas</h2>
           <form>
             <div className="input">
-              <IoPerson /> <input type="text" placeholder="Ime" />
+              <IoPerson /> <input type="text" placeholder="Ime" value={name} onChange={e=> setName(e.target.value)}/>
             </div>
             <div className="input">
               <IoMail /> <input type="email" placeholder="Email" />
@@ -29,7 +65,7 @@ const ContactPage = async () => {
             <div className="input">
               <textarea rows={4} placeholder="Poruka" />
             </div>
-            <button>Pošalji</button>
+            <button type="submit" onClick={handleFormSubmit}>Pošalji</button>
           </form>
         </div>
       </div>
@@ -38,3 +74,6 @@ const ContactPage = async () => {
 };
 
 export default ContactPage;
+
+
+
