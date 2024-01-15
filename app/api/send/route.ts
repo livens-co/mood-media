9dca7cd532287c4d6a82e9603cd89c29ec521464
dcvijetovic <dominik@mintandlime.eu>
2024-01-15 17:26:33 +0100

    resend

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,22 +1,22 @@
-import { ContactTemplate } from "@/components/EmailTemplates/ContactTemplate";
-import { Resend } from "resend";
+// import { ContactTemplate } from "@/components/EmailTemplates/ContactTemplate";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Mood Media <noreply@moodmedia.hr>',
-      to: 'dominik@mintandlime.eu',
-      bcc: 'dominik@mintandlime.eu',
-      subject: 'Upit zaprimljen',
-      react: ContactTemplate({ firstName: 'John' }),
-      text: ''
-    });
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Mood Media <noreply@moodmedia.hr>',
+//       to: 'dominik@mintandlime.eu',
+//       bcc: 'dominik@mintandlime.eu',
+//       subject: 'Upit zaprimljen',
+//       react: ContactTemplate({ firstName: 'John' }),
+//       text: ''
+//     });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
 
