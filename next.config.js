2923ea56566bb09848713783f46c313194d56af6
dcvijetovic <dominik@mintandlime.eu>
2024-01-18 18:20:03 +0100

    rewrites

--- next.config.js
+++ next.config.js
@@ -10,6 +10,78 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/sead",
+        destination: "/kreatori/sead-hrustanovic",
+      },
+      {
+        source: "/marco",
+        destination: "/kreatori/marco-cuccurin",
+      },
+      {
+        source: "/mirsad",
+        destination: "/kreatori/mirsad-kadic",
+      },
+      {
+        source: "/nika",
+        destination: "/kreatori/nika-pavicic",
+      },
+      {
+        source: "/rafaela",
+        destination: "/kreatori/rafaela-seba",
+      },
+      {
+        source: "/andrea",
+        destination: "/kreatori/andrea-pacadi",
+      },
+      {
+        source: "/pave",
+        destination: "/kreatori/pave-elez",
+      },
+      {
+        source: "/monika",
+        destination: "/kreatori/monika-zilavi",
+      },
+      {
+        source: "/josip-p",
+        destination: "/kreatori/josip-prepelic",
+      },
+      {
+        source: "/mia",
+        destination: "/kreatori/mia-negovetic",
+      },
+      {
+        source: "/leon",
+        destination: "/kreatori/leon-kosanovic",
+      },
+      {
+        source: "/josip",
+        destination: "/kreatori/josip-bacic",
+      },
+      {
+        source: "/mateo",
+        destination: "/kreatori/mateo-elez",
+      },
+      {
+        source: "/tibor",
+        destination: "/kreatori/tibor-cubrilo",
+      },
+      {
+        source: "/vedran",
+        destination: "/kreatori/vedran-kantar",
+      },
+      {
+        source: "/zoe",
+        destination: "/kreatori/zoe-bakalov",
+      },
+      {
+        source: "/magdalena",
+        destination: "/kreatori/magdalena-folnozic",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
