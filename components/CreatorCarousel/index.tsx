67104a42f6d4dd88ecf17ec9e3cb710595cf5be6
dcvijetovic <dominik@mintandlime.eu>
2024-02-01 13:46:25 +0100

    changes

--- components/CreatorCarousel/index.tsx
+++ components/CreatorCarousel/index.tsx
@@ -6,6 +6,14 @@ import Image from "next/image";
 
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "swiper/css/effect-creative";
+
+// import required modules
+import { EffectCards, EffectCreative } from "swiper/modules";
 
 interface Creator {
   slug: string;
@@ -18,55 +26,116 @@ interface HouseCarouselProps {
 }
 
 const CreatorCarousel: React.FC<HouseCarouselProps> = ({ creators }) => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [touchPosition, setTouchPosition] = useState<number | null>(null);
+  // const [touchPosition, setTouchPosition] = useState<number | null>(null);
 
-  const handleTouchStart = (e: React.TouchEvent) => {
-    const touchDown = e.touches[0].clientX;
-    setTouchPosition(touchDown);
-  };
+  // const handleTouchStart = (e: React.TouchEvent) => {
+  //   const touchDown = e.touches[0].clientX;
+  //   setTouchPosition(touchDown);
+  // };
 
-  const handleTouchMove = (e: React.TouchEvent) => {
-    const touchDown = touchPosition;
+  // const handleTouchMove = (e: React.TouchEvent) => {
+  //   const touchDown = touchPosition;
 
-    if (touchDown === null) {
-      return;
-    }
+  //   if (touchDown === null) {
+  //     return;
+  //   }
 
-    const currentTouch = e.touches[0].clientX;
-    const diff = touchDown - currentTouch;
+  //   const currentTouch = e.touches[0].clientX;
+  //   const diff = touchDown - currentTouch;
 
-    if (diff > 5) {
-      handleNextClick();
-    }
+  //   if (diff > 5) {
+  //     handleNextClick();
+  //   }
 
-    if (diff < -5) {
-      handleBackClick();
-    }
+  //   if (diff < -5) {
+  //     handleBackClick();
+  //   }
 
-    setTouchPosition(null);
-  };
+  //   setTouchPosition(null);
+  // };
 
-  const handleBackClick = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? creators.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
+  // const handleBackClick = () => {
+  //   const isFirstSlide = currentIndex === 0;
+  //   const newIndex = isFirstSlide ? creators.length - 1 : currentIndex - 1;
+  //   setCurrentIndex(newIndex);
+  // };
 
-  const handleNextClick = () => {
-    const isLastSlide = currentIndex === creators.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const displayedCreators = creators.slice(
-    Number(currentIndex),
-    Number(currentIndex) + 1
-  );
+  // const handleNextClick = () => {
+  //   const isLastSlide = currentIndex === creators.length - 1;
+  //   const newIndex = isLastSlide ? 0 : currentIndex + 1;
+  //   setCurrentIndex(newIndex);
+  // };
 
   return (
     <>
+      {/* <Swiper effect={"cards"} grabCursor={true} modules={[EffectCards]}  slidesPerView={1} spaceBetween={20}> */}
+      <Swiper
+        grabCursor={true}
+        effect={"creative"}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        }}
+        modules={[EffectCreative]}
+        className="mySwiper"
+      >
+        {creators.map((c) => (
+          <SwiperSlide key={c.slug} className="custom-slide">
+            <Link href={`/kreatori/${c.slug}`} className="creatorCarousel">
+              <Image
+                src={c.avatar}
+                alt={c.name}
+                width={400}
+                height={400}
+                className="carouselImage"
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="swipeArrow">
+        <FaArrowRight />
+      </div>
+      {/* {displayedCreators.map((c) => (
+        <Link
+          href={`/kreatori/${c.slug}`}
+          key={c.slug}
+          className="creatorCarousel"
+          // onTouchStart={handleTouchStart}
+          // onTouchMove={handleTouchMove}
+         
+        >
+          <Image
+            src={c.avatar}
+            alt={c.name}
+            width={288}
+            height={288}
+            className="carouselImage"
+          />
+        </Link>
+      ))} */}
+      {/* <div className="creatorNavigation">
+        <div className="navButton" onClick={handleBackClick}>
+          <FaArrowLeft />
+        </div>
+        <div className="navButton" onClick={handleNextClick}>
+          <FaArrowRight />
+        </div> */}
+      {/* </div> */}
+    </>
+  );
+};
+
+export default CreatorCarousel;
+
+{
+  /* <>
       {displayedCreators.map((c) => (
         <Link
           href={`/kreatori/${c.slug}`}
@@ -92,8 +161,5 @@ const CreatorCarousel: React.FC<HouseCarouselProps> = ({ creators }) => {
           <FaArrowRight />
         </div>
       </div>
-    </>
-  );
-};
-
-export default CreatorCarousel;
+    </> */
+}
