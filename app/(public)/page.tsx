c353e6e8d554b76b1b5b46cca7935b3e14aa0e1e
dcvijetovic <dominik@mintandlime.eu>
2024-03-04 10:55:15 +0100

    changes

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -35,7 +35,7 @@ const HomePage = async () => {
           loop={true}
           muted
           playsInline
-          src="/assets/moodMediaVideoMobile.mp4"
+          // src="/assets/moodMediaVideoMobile.mp4"
         >
           <source
             src="/assets/moodMediaVideoMobile.mp4"
