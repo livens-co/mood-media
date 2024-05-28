0b9b5172eb73354b3ed721cc740768d3525c471e
dcvijetovic <dominik@mintandlime.eu>
2024-05-28 22:07:35 +0200

    changes

--- app/(public)/klijenti/page.tsx
+++ app/(public)/klijenti/page.tsx
@@ -11,13 +11,9 @@ const ClientsPage = async () => {
 
   return (
     <div className="clientsPage">
-      <Row1 data={campaigns.slice(0, 7)} />
+      <Row1 data={campaigns.slice(0, 14)} />
 
-      <Row2 data={campaigns.slice(7, 14)} />
-
-      <Row1 data={campaigns.slice(14, 22)} />
-
-      <Row2 data={campaigns.slice(22, 28)} />
+      <Row2 data={campaigns.slice(14, 28)} />
 
       <Row1 data={campaigns.slice(28, -1)} />
     </div>
