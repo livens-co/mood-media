2e1fd5d18ae7378661d1d1680617f894cbeb1e21
dcvijetovic <dominik@mintandlime.eu>
2024-05-28 10:09:41 +0200

    changes

--- components/ClientPageRows/Row2.tsx
+++ components/ClientPageRows/Row2.tsx
@@ -1,10 +1,108 @@
+// "use client";
+
+// import ImageCard from "@/components/ImageCard";
+// import "./style.scss";
+// import useMeasure from "react-use-measure";
+// import { animate, useMotionValue, motion } from "framer-motion";
+// import { useEffect, useState } from "react";
+// import VideoModal from "@/components/VideoModal";
+// import { Campaign } from "@/types";
+
+// interface RowProps {
+//   data: Campaign[];
+// }
+
+// const Row2: React.FC<RowProps> = ({ data }) => {
+//   const FAST_DURATION = 20;
+//   const SLOW_DURATION = 70;
+
+//   const [duration, setDuration] = useState(FAST_DURATION);
+//   const [mustFinish, setMustFinish] = useState(false);
+//   const [rerender, setRerender] = useState(false);
+//   const [open, setOpen] = useState(false);
+//   const [selectedCampaign, setSelectedCampaign] = useState<Campaign | null>(
+//     null
+//   );
+
+//   let [ref, { width }] = useMeasure();
+//   const xTranslation = useMotionValue(0);
+
+//   const closeVideoModal = () => {
+//     setOpen(false);
+//   };
+
+//   useEffect(() => {
+//     let controls;
+//     let finalPosition = -width / 2 + 8;
+
+//     if (mustFinish) {
+//       controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
+//         ease: "linear",
+//         duration: duration * (1 - xTranslation.get() / finalPosition),
+//         onComplete: () => {
+//           setMustFinish(false);
+//           setRerender(!rerender);
+//         },
+//       });
+//     } else {
+//       controls = animate(xTranslation, [0, finalPosition], {
+//         ease: "linear",
+//         duration: duration,
+//         repeat: Infinity,
+//         repeatType: "loop",
+//         repeatDelay: 0,
+//       });
+//     }
+
+//     return controls?.stop;
+//   }, [xTranslation, width, duration, rerender, mustFinish]);
+
+//   const handleImageCardClick = (campaign: Campaign) => {
+//     setSelectedCampaign(campaign);
+//     setOpen(true);
+//   };
+
+//   return (
+//     <div className="clientRow">
+//       <motion.div
+//         className="row"
+//         ref={ref}
+//         style={{ x: xTranslation }}
+//         onHoverStart={() => {
+//           setMustFinish(true);
+//           setDuration(SLOW_DURATION);
+//         }}
+//         onHoverEnd={() => {
+//           setMustFinish(true);
+//           setDuration(FAST_DURATION);
+//         }}
+//       >
+//         {[...data, ...data, ...data].map((c) => (
+//           <div key={c._id} onClick={() => handleImageCardClick(c)}>
+//             <ImageCard image={c.image} />
+//           </div>
+//         ))}
+//       </motion.div>
+
+//       {open && selectedCampaign && (
+//         <VideoModal
+//           modalOpen={open}
+//           handleClose={closeVideoModal}
+//           video={selectedCampaign.playbackId}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Row2;
+
 "use client";
 
+import React, { useState } from "react";
 import ImageCard from "@/components/ImageCard";
 import "./style.scss";
 import useMeasure from "react-use-measure";
-import { animate, useMotionValue, motion } from "framer-motion";
-import { useEffect, useState } from "react";
 import VideoModal from "@/components/VideoModal";
 import { Campaign } from "@/types";
 
@@ -13,76 +111,28 @@ interface RowProps {
 }
 
 const Row2: React.FC<RowProps> = ({ data }) => {
-  const FAST_DURATION = 20;
-  const SLOW_DURATION = 70;
-
-  const [duration, setDuration] = useState(FAST_DURATION);
-  const [mustFinish, setMustFinish] = useState(false);
-  const [rerender, setRerender] = useState(false);
   const [open, setOpen] = useState(false);
-  const [selectedCampaign, setSelectedCampaign] = useState<Campaign | null>(
-    null
-  );
-
-  let [ref, { width }] = useMeasure();
-  const xTranslation = useMotionValue(0);
-
-  const closeVideoModal = () => {
-    setOpen(false);
-  };
-
-  useEffect(() => {
-    let controls;
-    let finalPosition = -width / 2 + 8;
-
-    if (mustFinish) {
-      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
-        ease: "linear",
-        duration: duration * (1 - xTranslation.get() / finalPosition),
-        onComplete: () => {
-          setMustFinish(false);
-          setRerender(!rerender);
-        },
-      });
-    } else {
-      controls = animate(xTranslation, [0, finalPosition], {
-        ease: "linear",
-        duration: duration,
-        repeat: Infinity,
-        repeatType: "loop",
-        repeatDelay: 0,
-      });
-    }
-
-    return controls?.stop;
-  }, [xTranslation, width, duration, rerender, mustFinish]);
+  const [selectedCampaign, setSelectedCampaign] = useState<Campaign | null>(null);
+  const [ref] = useMeasure();
 
   const handleImageCardClick = (campaign: Campaign) => {
     setSelectedCampaign(campaign);
     setOpen(true);
   };
 
+  const closeVideoModal = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="clientRow">
-      <motion.div
-        className="row"
-        ref={ref}
-        style={{ x: xTranslation }}
-        onHoverStart={() => {
-          setMustFinish(true);
-          setDuration(SLOW_DURATION);
-        }}
-        onHoverEnd={() => {
-          setMustFinish(true);
-          setDuration(FAST_DURATION);
-        }}
-      >
+    <div className="clientRow" ref={ref}>
+      <div className="row row2">
         {[...data, ...data, ...data].map((c) => (
-          <div key={c._id} onClick={() => handleImageCardClick(c)}>
+          <div key={c._id} className="imageContainer" onClick={() => handleImageCardClick(c)}>
             <ImageCard image={c.image} />
           </div>
         ))}
-      </motion.div>
+      </div>
 
       {open && selectedCampaign && (
         <VideoModal
